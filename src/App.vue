ef86f3b1300fd1819c35b7b51d09eabcb70efc48
Andreas Partsch <public@partsch.xyz>
2022-03-28 17:07:51 +0200

    Add color scheme

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,13 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+:root {
+  --color-blue--light: #85c0f9;
+  --color-blue: #268ff6;
+  --color-blue--dark: #041a56;
+  --color-grey--light: #f5f5f5;
+  --color-grey: #bbbbbb;
+  --color-grey--dark: #9a9797;
+}
+</style>
