47ac9bea1babc0029a17660db146404c183b4fb7
Andreas Partsch <public@partsch.xyz>
2022-03-28 17:21:35 +0200

    Add dices to board (wip)

--- src/components/KniffelBoard.vue
+++ src/components/KniffelBoard.vue
@@ -1,27 +1,47 @@
 <template>
   <div class="board-container">
-    <div class="board"></div>
+    <div class="board">
+      <div class="dice-container">
+        <kniffel-dice v-for="dice in numberOfDices" :key="dice" class="dice" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import KniffelDice from "@/components/KniffelDice";
 export default {
   name: "KniffelBoard",
+  components: { KniffelDice },
+  data() {
+    return {
+      numberOfDices: 5,
+      dices: [],
+    };
+  },
 };
 </script>
 
 <style scoped>
 .board-container {
-  width: 100%;
+  background: var(--color-grey--light);
   height: calc(100% - 36px);
   padding-top: 2rem;
-  background: var(--color-grey--light);
+  width: 100%;
 }
 .board {
-  height: 50px;
-  width: 100%;
+  background: white;
   border: 1px solid var(--color-grey);
   border-radius: 5px;
-  background: white;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+.dice-container {
+  display: flex;
+  margin: 1rem 0 0.5rem;
+}
+.dice:not(:last-of-type) {
+  margin-right: 1rem;
 }
 </style>
---
+++ src/components/KniffelDice.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="dice"></div>
+</template>
+
+<script>
+export default {
+  name: "KniffelDice",
+};
+</script>
+
+<style scoped>
+.dice {
+  width: 50px;
+  height: 50px;
+  border-radius: 10px;
+  background: var(--color-blue--light);
+}
+</style>
